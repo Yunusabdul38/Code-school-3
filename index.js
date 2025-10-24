d027dde2b0001425b7ef41c4de8f593e75e829cf
martinvibes <martinmachiebe21@gmail>
2025-10-24 14:42:43 +0100

    if and else and ternery operator

---
+++ index.js
@@ -0,0 +1,64 @@
+// alert("Hello");
+// alert("world");
+// const NUM = "100";
+// let num = 10;
+// let num2 = NUM * 2;
+
+// myBirthday = "01/12/1947";
+
+// let name;
+// name = "Victoria";
+
+// console.log(NUM);
+// console.log(num2);
+// alert(name);
+
+// let number1; // Number
+// let string = "Hello World"; // String
+// let biggInt = 9007199254740992n; // BigInt
+// let bool = true; // Boolean
+// let nullVar = null; // Null
+// let undef = undefined; // Undefined
+
+// console.log(number1);
+
+// console.log("This is the limit for a number in JS", number1);
+
+// let user_name = prompt("What is your name?", "Type your name here");
+
+// console.log(user_name);
+// alert(`Hello ${user_name}, Welcome my website `);
+// alert("hello " + user_name)
+
+let name = "Victoria";
+console.log(name);
+
+let num1 = 1;
+// let num2 = num1 + 1;
+// num1++;
+// num1 -= 1;
+
+console.log(num1);
+
+let a = 7;
+let b = 6;
+
+console.log(false >= 0);
+
+if (-4 == false) {
+  console.log("Hello it is true");
+} else {
+  console.log("The value is false");
+}
+
+let year = prompt("How old are you", "");
+
+if (year < 18) {
+  alert("You are too young");
+} else if (year > 18 && year < 40) {
+  alert("You are an adult");
+} else if (year >= 40 && year < 95) {
+  alert("You are mature");
+} else {
+  alert("Otilo");
+}
