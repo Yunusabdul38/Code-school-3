440a0288c63c08bf61b76ff4a7ef9c1919a40f36
martinvibes <martinmachiebe21@gmail>
2025-11-07 12:43:16 +0100

    js

--- index.js
+++ index.js
@@ -51,14 +51,76 @@ if (-4 == false) {
   console.log("The value is false");
 }
 
-let year = prompt("How old are you", "");
+// let year = prompt("How old are you", "");
 
-if (year < 18) {
-  alert("You are too young");
-} else if (year > 18 && year < 40) {
-  alert("You are an adult");
-} else if (year >= 40 && year < 95) {
-  alert("You are mature");
-} else {
-  alert("Otilo");
+// if (year < 18) {
+//   alert("You are too young");
+// } else if (year > 18 && year < 40) {
+//   alert("You are an adult");
+// } else if (year >= 40 && year < 95) {
+//   alert("You are mature");
+// } else {
+//   alert("Otilo");
+// }
+
+// let time = prompt("What time is it now", "");
+
+// if (time < 8) {
+//   alert("Good morning, you came early");
+// } else if (time >= 8 && time < 10) {
+//   alert("Class has started");
+// } else if (time == 10) {
+//   alert("Break time");
+// } else if (time == 10.3) {
+//   alert("break over Class resumed");
+// } else if (time > 10.3 && time < 14) {
+//   alert("We are still in class");
+// } else if (time == 14) {
+//   alert("Closing time");
+// } else {
+//   alert("We did not see you today");
+// }
+
+// let schoolTime =
+//   time < 8
+//     ? alert("Good morning, you came earlyGood morning")
+//     : time >= 8 && time < 10
+//     ? alert("class has started")
+//     : time == 10
+//     ? alert("break time")
+//     : time == 10.3
+//     ? alert("break over Class resumed")
+//     : time > 10.3 && time < 14
+//     ? alert("We are still in class")
+//     : time == 14
+//     ? alert("Closing time")
+//     : alert("We did not see you today");
+
+// schoolTime;
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// let i = 1;
+// while (i < 8) {
+//   console.log(i + " day of the week");
+//   i += 3;
+// }
+
+// let num = 10;
+// do {
+//   console.log("Number is " + num);
+//   num++;
+// } while (num < 5);
+
+for (let i = 0; i < 8; i++) {
+  console.log(i + " iteration");
 }
