c3b397447c5c88378205d1341eaf58be1a4f6c2f
martinvibes <martinmachiebe21@gmail>
2025-11-12 15:06:14 +0100

    add error handling

--- form.js
+++ form.js
@@ -4,6 +4,9 @@ let message = document.getElementById("message");
 let btn = document.getElementById("btn");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
+let nameErr = document.getElementById("name-err");
+let emailErr = document.getElementById("email-err");
+let mesgErr = document.getElementById("msg-err");
 
 function saveToLocalStorage() {
   let nameInput = name.value;
@@ -15,6 +18,25 @@ function saveToLocalStorage() {
     email: emailInput,
     message: MessageInput,
   };
+
+  if (nameInput == "") {
+    // alert("Please enter your name");
+    nameErr.innerText = "Please enter your name";
+    nameErr.style.display = "block";
+    return;
+  } else if (emailInput == "") {
+    // alert("Please enter your email");
+    emailErr.innerText = "Please enter your email";
+    emailErr.style.display = "block";
+    return;
+  } else if (MessageInput == "") {
+    // alert("pls enter your message");
+    mesgErr.innerText = "Please enter your message";
+    mesgErr.style.display = "block";
+    return;
+  } else {
+    alert("You have submitted successfully ✅");
+  }
 
   localStorage.setItem("userData", JSON.stringify(mainData));
 
